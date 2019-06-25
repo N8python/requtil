968d309be98aa2e0a8f5d1cbbb42dd3bac1be423
n8programs <43304488+N8python@users.noreply.github.com>
2019-06-25 12:20:38 -0400

    Create requtil.java

---
+++ java/requtil.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.function.*;
+class Requtil {
+  private static int index = 0;
+  static <T> void  sequence (List<Requestor<T>> seq, T initial){
+    Thread sequenceThread = new Thread(
+      () -> {
+        Function<T, Void> pass = new Function<T, Void>() {
+            public Void apply(T value) {
+              index += 1;
+              if(index >= seq.size()){
+                index = 0;
+                return null;
+              }
+              seq.get(index).resolve(this::apply, value);
+              return null;
+            }
+        };
+        seq.get(index).resolve(pass, initial);
+      }
+    );
+    sequenceThread.start();
+  }
+}
